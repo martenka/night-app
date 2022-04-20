e6c0ae4d622417798ddcc3f09f437076fe5f1ca5
martenka <marten.kahu.001@gmail.com>
2022-04-20 20:14:39 +0300

    chore: Update React to V18 and install Leaflet

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOMClient from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const rootContainer = document.getElementById('root') as HTMLElement;
+const root = ReactDOMClient.createRoot(rootContainer);
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-    document.getElementById('root') as HTMLElement
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
